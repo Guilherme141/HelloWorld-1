d2e97a4582a679fb377363b830d913450bdca003
Nilton-3923 <64760713+Nilton-3923@users.noreply.github.com>
2022-03-22 13:40:22 -0300

    Adicionando banco de dados

--- bdhelloworld.sql
+++ bdhelloworld.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 20-Mar-2022 às 05:21
+-- Tempo de geração: 22-Mar-2022 às 17:39
 -- Versão do servidor: 10.4.22-MariaDB
 -- versão do PHP: 8.1.2
 
@@ -20,6 +20,149 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `bdhelloworld`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tbagenda`
+--
+
+CREATE TABLE `tbagenda` (
+  `idAgenda` int(11) NOT NULL,
+  `nomeAgenda` varchar(30) DEFAULT NULL,
+  `dataAgenda` datetime DEFAULT NULL,
+  `horarioTerminoAgenda` time DEFAULT NULL,
+  `horarioAgenda` time DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tbagendaatividade`
+--
+
+CREATE TABLE `tbagendaatividade` (
+  `idAgendaAtividade` int(11) NOT NULL,
+  `dataVencimentoAtividade` date DEFAULT NULL,
+  `dataAdicionadaAtividade` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tbarquivoatividade`
+--
+
+CREATE TABLE `tbarquivoatividade` (
+  `idArquivoAtividade` int(11) NOT NULL,
+  `tipoArquivoAtividade` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tbatividade`
+--
+
+CREATE TABLE `tbatividade` (
+  `idAtividade` int(11) NOT NULL,
+  `tituloAtividade` varchar(50) DEFAULT NULL,
+  `textoAtividade` varchar(200) DEFAULT NULL,
+  `idUsuario` int(11) DEFAULT NULL,
+  `idArquivoAtividade` int(11) NOT NULL,
+  `idTurma` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tbaula`
+--
+
+CREATE TABLE `tbaula` (
+  `idAula` int(11) NOT NULL,
+  `tipoAula` varchar(40) DEFAULT NULL,
+  `idMaterial` int(11) DEFAULT NULL,
+  `idAgenda` int(11) DEFAULT NULL,
+  `idTurma` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tbchat`
+--
+
+CREATE TABLE `tbchat` (
+  `idChat` int(11) NOT NULL,
+  `dataChat` date DEFAULT NULL,
+  `horaChat` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `textoChat` varchar(1000) DEFAULT NULL,
+  `idTipoUsuario` int(11) DEFAULT NULL,
+  `idContChat` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tbcontchat`
+--
+
+CREATE TABLE `tbcontchat` (
+  `idContChat` int(11) NOT NULL,
+  `tipoArquivoContChat` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tbmaterial`
+--
+
+CREATE TABLE `tbmaterial` (
+  `idMaterial` int(11) NOT NULL,
+  `tituloMaterial` varchar(50) DEFAULT NULL,
+  `nomeMaterial` varchar(100) DEFAULT NULL,
+  `dataMaterial` datetime DEFAULT NULL,
+  `conteudoMaterial` varchar(200) DEFAULT NULL,
+  `idAgenda` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tbtipousuario`
+--
+
+CREATE TABLE `tbtipousuario` (
+  `idTipoUsuario` int(11) NOT NULL,
+  `descRipoUsuario` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tbturma`
+--
+
+CREATE TABLE `tbturma` (
+  `idTurma` int(11) NOT NULL,
+  `siglaTurma` varchar(3) DEFAULT NULL,
+  `semestreTurma` varchar(20) NOT NULL,
+  `idUsuario` int(11) NOT NULL,
+  `nomeTurma` varchar(255) DEFAULT NULL,
+  `descTurma` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `tbturma`
+--
+
+INSERT INTO `tbturma` (`idTurma`, `siglaTurma`, `semestreTurma`, `idUsuario`, `nomeTurma`, `descTurma`) VALUES
+(1, 'a', '2', 1, 'Banco de Dados', 'Turma de Banco'),
+(2, 'b', '3', 1, 'Pw3', 'Turma de Pw3'),
+(3, 'a', '2', 2, 'PTCC', 'Turma de TCC'),
+(4, 'a &', '2', 3, 'Desenvolvimento de Sistemas', 'Turma de desenvolvimento de sistemas');
 
 -- --------------------------------------------------------
 
@@ -50,6 +193,76 @@ INSERT INTO `tbusuario` (`idUsuario`, `nomeUsuario`, `emailUsuario`, `loginUsuar
 --
 
 --
+-- Índices para tabela `tbagenda`
+--
+ALTER TABLE `tbagenda`
+  ADD PRIMARY KEY (`idAgenda`);
+
+--
+-- Índices para tabela `tbagendaatividade`
+--
+ALTER TABLE `tbagendaatividade`
+  ADD PRIMARY KEY (`idAgendaAtividade`);
+
+--
+-- Índices para tabela `tbarquivoatividade`
+--
+ALTER TABLE `tbarquivoatividade`
+  ADD PRIMARY KEY (`idArquivoAtividade`);
+
+--
+-- Índices para tabela `tbatividade`
+--
+ALTER TABLE `tbatividade`
+  ADD PRIMARY KEY (`idAtividade`),
+  ADD KEY `idUsuario` (`idUsuario`),
+  ADD KEY `idArquivoAtividade` (`idArquivoAtividade`),
+  ADD KEY `idTurma` (`idTurma`);
+
+--
+-- Índices para tabela `tbaula`
+--
+ALTER TABLE `tbaula`
+  ADD PRIMARY KEY (`idAula`),
+  ADD KEY `idMaterial` (`idMaterial`),
+  ADD KEY `idAgenda` (`idAgenda`),
+  ADD KEY `idTurma` (`idTurma`);
+
+--
+-- Índices para tabela `tbchat`
+--
+ALTER TABLE `tbchat`
+  ADD PRIMARY KEY (`idChat`),
+  ADD KEY `idTipoUsuario` (`idTipoUsuario`),
+  ADD KEY `idContChat` (`idContChat`);
+
+--
+-- Índices para tabela `tbcontchat`
+--
+ALTER TABLE `tbcontchat`
+  ADD PRIMARY KEY (`idContChat`);
+
+--
+-- Índices para tabela `tbmaterial`
+--
+ALTER TABLE `tbmaterial`
+  ADD PRIMARY KEY (`idMaterial`),
+  ADD KEY `idAgenda` (`idAgenda`);
+
+--
+-- Índices para tabela `tbtipousuario`
+--
+ALTER TABLE `tbtipousuario`
+  ADD PRIMARY KEY (`idTipoUsuario`);
+
+--
+-- Índices para tabela `tbturma`
+--
+ALTER TABLE `tbturma`
+  ADD PRIMARY KEY (`idTurma`),
+  ADD KEY `tbturma_ibfk_1` (`idUsuario`);
+
+--
 -- Índices para tabela `tbusuario`
 --
 ALTER TABLE `tbusuario`
@@ -61,6 +274,66 @@ ALTER TABLE `tbusuario`
 --
 
 --
+-- AUTO_INCREMENT de tabela `tbagenda`
+--
+ALTER TABLE `tbagenda`
+  MODIFY `idAgenda` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `tbagendaatividade`
+--
+ALTER TABLE `tbagendaatividade`
+  MODIFY `idAgendaAtividade` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `tbarquivoatividade`
+--
+ALTER TABLE `tbarquivoatividade`
+  MODIFY `idArquivoAtividade` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `tbatividade`
+--
+ALTER TABLE `tbatividade`
+  MODIFY `idAtividade` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `tbaula`
+--
+ALTER TABLE `tbaula`
+  MODIFY `idAula` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `tbchat`
+--
+ALTER TABLE `tbchat`
+  MODIFY `idChat` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `tbcontchat`
+--
+ALTER TABLE `tbcontchat`
+  MODIFY `idContChat` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `tbmaterial`
+--
+ALTER TABLE `tbmaterial`
+  MODIFY `idMaterial` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `tbtipousuario`
+--
+ALTER TABLE `tbtipousuario`
+  MODIFY `idTipoUsuario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `tbturma`
+--
+ALTER TABLE `tbturma`
+  MODIFY `idTurma` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT de tabela `tbusuario`
 --
 ALTER TABLE `tbusuario`
@@ -69,6 +342,41 @@ ALTER TABLE `tbusuario`
 --
 -- Restrições para despejos de tabelas
 --
+
+--
+-- Limitadores para a tabela `tbatividade`
+--
+ALTER TABLE `tbatividade`
+  ADD CONSTRAINT `tbatividade_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `tbusuario` (`idUsuario`),
+  ADD CONSTRAINT `tbatividade_ibfk_2` FOREIGN KEY (`idArquivoAtividade`) REFERENCES `tbarquivoatividade` (`idArquivoAtividade`),
+  ADD CONSTRAINT `tbatividade_ibfk_3` FOREIGN KEY (`idTurma`) REFERENCES `tbturma` (`idTurma`);
+
+--
+-- Limitadores para a tabela `tbaula`
+--
+ALTER TABLE `tbaula`
+  ADD CONSTRAINT `idAgenda` FOREIGN KEY (`idAgenda`) REFERENCES `tbagenda` (`idAgenda`),
+  ADD CONSTRAINT `idMaterial` FOREIGN KEY (`idMaterial`) REFERENCES `tbmaterial` (`idMaterial`),
+  ADD CONSTRAINT `tbaula_ibfk_1` FOREIGN KEY (`idTurma`) REFERENCES `tbturma` (`idTurma`);
+
+--
+-- Limitadores para a tabela `tbchat`
+--
+ALTER TABLE `tbchat`
+  ADD CONSTRAINT `idContChat` FOREIGN KEY (`idContChat`) REFERENCES `tbcontchat` (`idContChat`),
+  ADD CONSTRAINT `idTipoUsuario` FOREIGN KEY (`idTipoUsuario`) REFERENCES `tbtipousuario` (`idTipoUsuario`);
+
+--
+-- Limitadores para a tabela `tbmaterial`
+--
+ALTER TABLE `tbmaterial`
+  ADD CONSTRAINT `tbmaterial_ibfk_1` FOREIGN KEY (`idAgenda`) REFERENCES `tbagenda` (`idAgenda`);
+
+--
+-- Limitadores para a tabela `tbturma`
+--
+ALTER TABLE `tbturma`
+  ADD CONSTRAINT `tbturma_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `tbusuario` (`idUsuario`);
 
 --
 -- Limitadores para a tabela `tbusuario`
