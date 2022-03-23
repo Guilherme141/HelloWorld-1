2355b1919fd21a314ae970102d3e8e519f2e5d1a
Guilherme de Lima Moura <79373128+Guilherme141@users.noreply.github.com>
2022-03-22 21:25:04 -0300

    Update Usuario.php

--- classe/Usuario.php
+++ classe/Usuario.php
@@ -58,5 +58,44 @@
                 $conexao->exec($queryInsert);
                 return 'Cadastro realizado com sucesso';
         }
+          //Método para Listar os Usuario (SELECT)
+        public function listar(){
+            $conexao = Conexao::pegarConexao();
+            $querySelect = "SELECT idusuario, nomeusuario, emailusuario, senhausuario FROM tbusuario";
+            $resultado = $conexao->query($querySelect);
+            $lista = $resultado->fetchAll();
+            return $lista;   
+        }
+        
+        //Método para Deletar usuarios (DELETE)
+        public function deletar(){
+            $conexao = Conexao::pegarConexao();
+            $id=$_GET['idusuario']; //Recupera o id do usuario cadastrado
+            $queryDelete = "DELETE FROM tbUsuario WHERE idusuario='$id'"; //deleta o usuario que conter o id
+            $conexao->exec($queryDelete);
+            header("Location: ../session/adm-restrita.php");
+            
+        }
+
+        //Método para Alterar dados(UPDATE)
+        public function alterar(){
+            $conexao = Conexao::pegarConexao();
+
+            //recuperar valores do form
+            $id=$_POST['idusuario'];
+            $nome=$_POST['nomeusuario'];
+            $email=$_POST['emailusuario'];
+            $senha=$_POST['senhausuario'];
+
+            //código update
+            $queryUpdate = "update tbusuario set 
+            nomeusuario='$nome'
+           ,emailusuario='$email'
+           ,senhausuario='$senha'
+           where idusuario='$id'";
+           $conexao->exec($queryUpdate);
+           
+           header("Location: ../session/adm-restrita.php");
+        }
     }
 ?>
